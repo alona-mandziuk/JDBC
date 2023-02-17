ad4519f17e269854f2ef34fee19a6e5111201d95
Alona Mandziuk <alona.mandziuk@privatbank.ua>
2023-02-17 18:51:00 +0200

    Initial commit

---
+++ src/hw1/TestJDBC.java
@@ -0,0 +1,50 @@
+package hw1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+class TestJDBC {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/zoomarket";
+    private static final String LOGIN = "root";
+    private static final String PASSWORD = "21032011philia";
+
+    public static void main(String[] args) {
+        registerDriver();
+        Connection connection = null;
+        Statement statement = null;
+
+        try {
+            connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+            statement = connection.createStatement();
+            statement.execute("insert into assortment (product_name, price) " +
+                    "values ('Bouncies', 3)");
+            statement.addBatch("insert into assortment (product_name, price)" +
+                    "values ('Fish Stripes', 2.5)");
+            statement.addBatch("insert into assortment (product_name, price)" +
+                    "values ('Treats with Lamb flavour', 4.5)");
+            statement.executeBatch();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void registerDriver() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.printf("Driver has successfully loaded!");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/hw1/zoomarket.sql
@@ -0,0 +1,6 @@
+create table assortment (
+id int(11) primary key not null auto_increment,
+product_name varchar (50) not null,
+definition varchar (50),
+price double (5,2)
+)
