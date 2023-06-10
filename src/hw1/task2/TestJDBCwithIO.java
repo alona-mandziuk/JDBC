003efa94507231c494d2fc912cf342be190f2387
Alona Mandziuk <alona.mandziuk@privatbank.ua>
2023-06-10 22:59:59 +0300

    initial commit

--- src/hw1/task2/TestJDBCwithIO.java
+++ src/hw1/task2/TestJDBCwithIO.java
@@ -14,28 +14,18 @@ class TestJDBCwithIO {
 
     public static void main(String[] args) throws IOException {
         String dataQueries = new String(Files.readAllBytes
-                (Paths.get("G:\\_java cbs\\homeWork\\5_JDBC&Hibernate\\JDBC_and_Hibernate\\src\\hw1\\task2\\QueriesToZoomarket.txt")));
+                (Paths.get("src/hw1/task2/QueriesToZoomarket.txt")));
         String[] queries = dataQueries.split("\\n");
 
-
         RegisterDriver.registerDriver();
-        Connection connection = null;
-        Statement statement = null;
-        try {
-            connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
-            statement = connection.createStatement();
+
+        try (Connection connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+             Statement statement = connection.createStatement()) {
             for (int i = 0; i < queries.length; i++) {
                 statement.execute(queries[i]);
             }
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                connection.close();
-                statement.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
